eee568746a15ba45240c14e4dba67ab021277fef
Elanjoeforte <Joseph2brasana@gmail.com>
2023-02-17 17:16:59 -0800

    task6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'o' ; i <= '9' ; i++)
+		printf("%d", i);
+	return (0);
+}
