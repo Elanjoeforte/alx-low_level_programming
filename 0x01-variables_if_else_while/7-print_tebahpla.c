ba364b98a5f10ee7c26799b4b3b8e25aa637e60d
Elanjoeforte <Joseph2brasana@gmail.com>
2023-02-17 18:00:07 -0800

    task8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
