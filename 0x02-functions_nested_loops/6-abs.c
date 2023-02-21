8ee9e3def1ce3421bb8083e7c7804f1582d50ccd
Elanjoeforte <Joseph2brasana@gmail.com>
2023-02-21 15:12:07 -0800

    functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - checks the absolute value of the number
+ * @n: parameter to be checked
+ * Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
