3a37d401e08a9067bb92456c938adaac9d2a677a
Elanjoeforte <Joseph2brasana@gmail.com>
2023-02-21 02:45:26 -0800

    nested loops 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = "_putchar")
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
